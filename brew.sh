d30908a6643757cb59cf4680767598f906748b64
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-02-13 19:20:16 +0100

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+# from gh/karan/dotfiles
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew install bash-completion
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php55 --with-gmp
+
+# Install some CTF tools; see https://github.com/ctfs/write-ups.
+brew install nmap
+
+
+# Install other useful binaries.
+brew install git
+brew install imagemagick --with-webp
+brew install lua
+brew install unrar
+brew install python3
+brew install postgresql
+brew install mongodb
+brew install gcc
+
+# Install Node.js. Note: this installs `npm` too, using the recommended
+# installation method.
+brew install node
+
+# my utils
+brew install geoip
+brew install galen
+brew install exiftool
+brew install cpanminus
+
+# taps
+brew tap caskroom/cask
+#brew tap homebrew/completions
+#brew tap homebrew/dupes
+#brew tap homebrew/head-only
+#brew tap homebrew/php
+#brew tap homebrew/versions
+# brew tap karan/karan
+brew tap telemachus/desc
+
+# cask
+brew cask install mamp
+brew cask install jdownloader
+brew cask install vagrant
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
