43fb8aadf7c33c97aa45032f7b67f876afa299e1
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-03-27 19:48:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,4 +14,7 @@ git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 
 #
 git clone https://github.com/bling/vim-airline.git ~/.vim/bundle/vim-airline
+
+#
+git clone https://github.com/docunext/closetag.vim.git ~/.vim/bundle/closetag.vim
 # -------------------------------------------
