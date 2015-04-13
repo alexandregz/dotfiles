fc05bb6214ff98942b05fffc120a4aa4ebcdce2a
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-04-13 10:30:22 +0200

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -1,9 +1,27 @@
+#!/usr/bin/env bash
+#
+
 # all from:
 # https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 # https://github.com/paulmillr/dotfiles/blob/master/etc/osx.sh
 
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
 # Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # Reveal IP address, hostname, OS version, etc. when clicking the clock
 # in the login window
@@ -18,5 +36,38 @@ defaults write com.apple.appstore ShowDebugMenu -bool true
 # recover keys repetition
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# show all partitions with Disk Utility
-defaults write com.apple.DiskUtility DUDebugMenuEnabled 1
+###############################################################################
+# Address Book, Dashboard, iCal, TextEdit, and Disk Utility                   #
+###############################################################################
+
+# Enable the debug menu in Address Book
+defaults write com.apple.addressbook ABShowDebugMenu -bool true
+
+# Enable Dashboard dev mode (allows keeping widgets on the desktop)
+defaults write com.apple.dashboard devmode -bool true
+
+# Enable the debug menu in iCal (pre-10.8)
+defaults write com.apple.iCal IncludeDebugMenu -bool true
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+# Enable the debug menu in Disk Utility
+defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
+defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+
+###############################################################################
+# Google Chrome & Google Chrome Canary                                        #
+###############################################################################
+# Use the system-native print preview dialog
+defaults write com.google.Chrome DisablePrintPreview -bool true
+defaults write com.google.Chrome.canary DisablePrintPreview -bool true
+
+# Expand the print dialog by default
+defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+
