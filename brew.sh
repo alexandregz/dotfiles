fa1ea3a73b0fb28e4963a0c64d0efdf5f87ec65f
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-04-16 10:58:21 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -9,6 +9,10 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+
+echo $0
+
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
@@ -108,3 +112,5 @@ brew install git-open
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+echo "Done $0"
