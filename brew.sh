accacbd0a121ec3cfa25b7b6edf3477619618f39
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-05-16 12:06:39 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -10,6 +10,12 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
+if ! command -v brew > /dev/null 2>&1; then
+  echo "installing"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
 echo $0
 
 
