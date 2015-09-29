48b7c971bf7b72a8785b9d3ff5bd583b0826606a
Alexandre Espinosa Menor <aemenor@gmail.com>
2015-09-29 22:32:22 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -41,6 +41,7 @@ brew install gnu-sed --with-default-names
 # running `chsh`.
 brew install bash
 brew install bash-completion
+brew install tree
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
