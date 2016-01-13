ae95c17764c765f0287dcccd59245eb8d0d375cd
Alexandre Espinosa Menor <aemenor@gmail.com>
2016-01-13 17:38:08 +0100

    Create bitbar-plugins.pl

---
+++ bitbar-plugins.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+#
+# Use after install bitbar
+#
+use strict;
+use File::Basename;
+
+my @plugins_to_activate=qw(Dev/Vagrant/vagrant.30s.pl System/Battery/mouse.1m.sh Dev/Docker/docker-status.1m.sh);
+
+my $renamed = { 'vagrant.30s.pl' => 'vagrant.3m.pl' }; 
+
+my $DIRECTORY_DEVELOPMENT = '/Volumes/HD/Development';
+
+my $GITHUB_REPO = 'https://github.com/matryer/bitbar-plugins';
+
+#
+`defaults write com.matryer.BitBar pluginsDirectory '$DIRECTORY_DEVELOPMENT/bitbar-plugins/Enabled'`;
+
+chdir $DIRECTORY_DEVELOPMENT;
+
+`/usr/bin/env git clone "$GITHUB_REPO"`;
+
+foreach my $plugin(@plugins_to_activate) {
+        my $plugin_name = basename($plugin);
+        $plugin_name = $renamed->{$plugin_name} if(exists($renamed->{$plugin_name}));
+
+        print "Symlink $plugin_name to $DIRECTORY_DEVELOPMENT/bitbar-plugins/Enabled\n";
+        `/bin/ln -s "$DIRECTORY_DEVELOPMENT/bitbar-plugins/$plugin" $DIRECTORY_DEVELOPMENT/bitbar-plugins/Enabled/$plugin_name`; 
+}
+
+# restarting BitBar
+my $PID_BITBAR = `ps -Ac -o pid,comm | awk '/^ *[0-9]+ BitBar\$/ {print \$1}'`;
+`/bin/kill -9 $PID_BITBAR`;
+`/usr/bin/open -a BitBar`
