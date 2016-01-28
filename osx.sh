0739bca78327736883ce330714443dafc6e2d017
Alexandre Espinosa Menor <aemenor@gmail.com>
2016-01-28 15:38:53 +0100

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -12,7 +12,8 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-
+# install some libs used by Homebrew formulas
+xcode-select --install
 
 ###############################################################################
 # Finder                                                                      #
